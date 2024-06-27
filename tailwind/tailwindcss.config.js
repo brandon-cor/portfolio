1427dccfb88d048c4c6e31804e793b20a421cf5e
Brandon <15brandon51@gmail.com>
2024-06-27 13:26:30 -0700

    removing tutoring section and slightly edited the content

---
+++ tailwind/tailwindcss.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./Users/brandon/websites/tailwind-manage-landing/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
